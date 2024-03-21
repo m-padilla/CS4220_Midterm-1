76020e26bc6103edd5a961cf0ef9a157661ffd3f
m-padilla <mpadil96@calstatela.edu>
2024-03-21 11:04:50 -0700

    Created the api.js file that gets data from the meal database api. Created the .json file that contains the dependencies required for this project.

---
+++ api.js
@@ -0,0 +1,31 @@
+// The file to interact with the deck of cards api
+import axios from 'axios';
+import 'dotenv/config';
+
+// base is the most common part of the api url before the it is made dynamic
+const base = `https://www.themealdb.com/api/json/v1/${process.env.API_KEY}`;
+
+// www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+export async function getMealQuery (meal){
+     try{
+          const apiURL = `${base}/search.php?s=${meal}`;
+          const response = await axios.get(apiURL);
+          console.log(response.data);
+     }
+     catch (error){
+          console.error(error);
+     }
+}
+
+// www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+export async function getMeal (mealId){
+     try{
+          const apiURL = `${base}/lookup.php?i=${mealId}`;
+          const response = await axios.get(apiURL);
+          
+          console.log(response.data);
+     }
+     catch (error){
+          console.error(error);
+     }
+}
