e031166831d1f5424785034c456c6229c0f67c4a
runyiy <50193518+runyiy@users.noreply.github.com>
2024-03-31 14:38:18 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ export async function displaySearchHistory() {
 
     for (let historyIdx = 0; historyIdx < history.length; historyIdx++) {
         const entry = history[historyIdx];
-        console.log(`${historyIdx + 1}) "${entry.search}" with ${entry.resultCount} results`);
+        console.log(
+            `${historyIdx + 1}) "${entry.search}" with ${
+                entry.resultCount
+            } results`
+        );
     }
 
     console.log('= = = = = = = = = = = = = = = = = = = = = =\n');
@@ -34,7 +38,7 @@ export async function displaySearchHistory() {
  * @description prints the meal information in a user friendly manner.
  * @param {{strMeal: string, idMeal: string, strInstructions: string}[]} meals
  * @returns {void}
-**/
+ **/
 function _prettyPrint(meals) {
     /* for each meal in meals...
        display the name of the meal
@@ -66,7 +70,6 @@ function _prettyPrint(meals) {
     console.log('= = = = = = = = = = = = = = = = = = = = = =');
 }
 
-
 async function _recipePrompt(meals) {
     const displayMeals = meals.map((meal) => {
         return { name: `${meal.strMeal}`, value: meal.idMeal };
@@ -78,13 +81,19 @@ async function _recipePrompt(meals) {
     });
 }
 
-export async function cookRecipe(type, variable, cache) {
+export async function cookRecipe(type, variable, cache = false) {
     try {
+
         let mealQuery;
         
         //search the recipe and save data in the search_history.json
         mealQuery = await fetchRecipeFromAPI(type, variable);
         
+        //throw error if don't have recipe 
+        if (mealQuery === null) {
+            throw new Error(`Sorry, we don't have a recipe for ${variable}.`);
+        }
+
         //Prompts the user to select an item from the search results.
         const selectedMealId = await _recipePrompt(mealQuery.meals);
         
@@ -103,7 +112,7 @@ export async function cookRecipe(type, variable, cache) {
                     value: mealQuery
                 });
             } else {
-                //get value(recipe) from cache
+                //get value(recipe) from cache if found
                 mealQuery = mealQuery.value;
             }
         } else {
@@ -133,9 +142,13 @@ async function fetchRecipeFromAPI(type, variable) {
         } else {
             mealQuery = await api.searchById(variable);
         }
-        //throw error if don't have recipe
+        //save resultcount as 0, because null has no length, and return null
         if (mealQuery.meals === null) {
-            throw new Error(`Sorry, we don't have a recipe for ${variable}.`);
+            await db.create('search_history', {
+                search: variable.charAt(0).toUpperCase() + variable.slice(1).toLowerCase(),
+                resultCount: 0
+            });
+            return null;
         }
         
         await db.create('search_history', {
@@ -144,59 +157,7 @@ async function fetchRecipeFromAPI(type, variable) {
         });
 
         return mealQuery;
-};
-
-export async function cookRecipe(type, variable, cache = false) {
-    try {
-        let mealQuery;
-
-        // Check if cache option is enabled
-        if (cache) {
-            // Attempt to find the selected item in the search cache
-            mealQuery = await db.find('search_cache', variable);
-
-            if (!mealQuery) {
-                // If not found in the search cache, get the selected item from the API
-                mealQuery = await fetchRecipeFromAPI(type, variable);
-
-                // Save an entry in search_cache.json
-                await db.create('search_cache', { key: variable, value: mealQuery });
-            }
-        } else {
-            // If cache option is false, get the selected item from the API
-            mealQuery = await fetchRecipeFromAPI(type, variable);
-
-            // Save an entry in search_cache.json
-            await db.create('search_cache', { key: variable, value: mealQuery });
-        }
-
-        // Print the retrieved meal information
-        if (mealQuery.meals === null) {
-            throw new Error(`Sorry, we don't have a recipe for ${variable}.`);
-        }
-        _prettyPrint(mealQuery.meals);
-
     } catch (error) {
         console.log(error.message);
     }
-}
-// Gets the recipe from api using cache
-async function fetchRecipeFromAPI(type, variable) {
-    let mealQuery;
-    if (type === 'name') {
-        mealQuery = await api.searchByName(variable);
-    } else if (type === 'firstLetter') {
-        mealQuery = await api.searchByFirstLetter(variable);
-    } else {
-        mealQuery = await api.searchById(variable);
-    }
-    await db.create(
-      'search_history',
-      {search: variable.charAt(0).toUpperCase() + variable.slice(1).toLowerCase(), 
-      resultCount: mealQuery.meals.length}
-    );
-    const selectedMeal = await _recipePrompt(mealQuery.meals);
-    mealQuery = await api.searchById(selectedMeal);
-  
-    return mealQuery;
 }
